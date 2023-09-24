53546ce444201b85e1b6ab8cbc4e253dd404f1b1
ondal <hiondal@gmail.com>
2023-09-24 18:15:16 +0900

    fix

--- app.js
+++ app.js
@@ -56,6 +56,7 @@ app.use(function(req, res, next) {
 	if((typeof token == "undefined") || token == null) token = "";
 	
     if(token === "") {
+        util.log("### Empty access token ###");
     	res.redirect("/login");
     	next();
     	return;
